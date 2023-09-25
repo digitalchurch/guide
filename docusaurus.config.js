4fd67f38dfc78f2a47c00dc3632b971b729b0c72
Mark Tenney <mark@dgtl.church>
2023-09-25 19:43:44 -0400

    footer link fix

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -87,7 +87,7 @@ const config = {
             items: [
               {
                 label: 'Guide',
-                to: '/docs/intro',
+                to: '/intro',
               },
             ],
           },
